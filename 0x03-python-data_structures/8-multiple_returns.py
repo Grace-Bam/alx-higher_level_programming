81f281efecb5ffbf881d1eb33e0953b111d9c6f4
root <Grace-Bam>
2024-01-09 08:44:20 -0800

    python-data_structure

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+
 def multiple_returns(sentence):
-    len_sen = len(sentence)
-
-    if (len_sen == 0):
-        new_tuple = (len_sen, None)
+    if sentence != '':
+        first_char = sentence[0]
     else:
-        new_tuple = (len_sen, sentence[0])
-
-    return (new_tuple)
+        first_char = None
+    return (len(sentence), first_char)
